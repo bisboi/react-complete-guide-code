2796395a33eac8d6827b895d1ada9323569c41e9
bis_ <bence.i.solti@gmail.com>
2022-10-13 09:40:35 +0200

    start_more todo added

--- code/react-vs-vanilla-js-example/react/src/App.js
+++ code/react-vs-vanilla-js-example/react/src/App.js
@@ -4,7 +4,15 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo text='Learn React' />
+      <Todo text="Learn React" />
+      <Todo text="Learn React aight?" />
+      <Todo text="Learn React aight?" />
+      <Todo text="Learn React aight?" />
+      <Todo text="Learn React aight?" />
+      <Todo text="Learn React aight?" />
+      <Todo text="Learn React aight?" />
+      <Todo text="Learn React aight?" />
+      <Todo text="Learn React aight?" />
     </div>
   );
 }
